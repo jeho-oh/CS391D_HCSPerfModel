bf561e8673032edb82db618b73a72cc1b26da8bc
oleg zilman <zilmano@gmail.com>
2020-04-06 17:15:43 -0500

    add batch processing to nn_experimental, add plotting of loss by training steps

--- bin/run_nn.py
+++ bin/run_nn.py
@@ -10,6 +10,7 @@ import torch
 import matplotlib.pyplot as plt
 import argparse
 import numpy as np
+import math
 
 ## Local modules from ../lib
 from nn_experimental import ExperimentalNN
@@ -31,7 +32,9 @@ def parse_args(argv):
     return argParser.parse_args(argv)
     
 
-def calc_mse_loss(X,Y,nn_model,pprint=False):
+       
+
+def calc_epoch_mse_loss(X,Y,nn_model,pprint=False):
     sample_num = X.shape[0] 
     Y_pred = torch.zeros(sample_num)
     loss_per_sample = torch.zeros(sample_num)
@@ -45,35 +48,38 @@ def calc_mse_loss(X,Y,nn_model,pprint=False):
     total_loss = torch.norm((Y_pred-Y))/sample_num
     return total_loss,loss_per_sample.detach().numpy()
 
-def calc_mre_loss(X,Y,nn_model,pprint=False):
+
+def calc_epoch_mre_loss(X,Y,nn_model,pprint=False):
     sample_num = X.shape[0] 
     print(str(sample_num))
     Y_pred = torch.zeros(sample_num)
-    mre = torch.zeros(sample_num)
+    mre_per_sample = torch.zeros(sample_num)
     for i in range(sample_num):
         y_pred = nn_model.predict(X[i])
         Y_pred[i] = y_pred
-        mre[i] = abs((y_pred-Y[i])/Y[i])
+        mre_per_sample[i] = abs((y_pred-Y[i])/Y[i])*100
         if pprint:
-            logmsg(" prediced Y:{}, Real Y:{} loss={}".format(mre[i])) 
+            logmsg(" prediced Y:{}, Real Y:{} loss={}".format(mre_per_sample[i])) 
         
-    total_loss = mre.sum()*(100/sample_num)
-    return total_loss,mre.detach().numpy()
+    total_mre_loss = mre_per_sample.sum()/sample_num
+    return total_mre_loss,mre_per_sample.detach().numpy()
 
-def plot_loss(test_loss,train_loss):
+#TODO: add MRE loss to pytorch model and train on it.
+def plot_mse_loss(test_loss,train_loss,xlabel,ylabel,
+                  title="Training Loss Progress"):
     ig, ax = plt.subplots(figsize=(12, 7))
     test_loss = test_loss.flatten()
     train_loss = train_loss.flatten()
     iterations = np.arange(train_loss.shape[0])
-    ax.set_title("Training loss progress")
-    ax.set_xlabel('Loss')
-    ax.set_ylabel('Steps')
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
     ax.plot(iterations,
             test_loss,
             color="b",label="test loss")
     ax.plot(iterations,
             pow(train_loss,0.5),
-            color="r",label="train_loss")
+            color="r",label="train loss")
     ax.legend()
     plt.show()
 
@@ -81,9 +87,11 @@ def plot_loss(test_loss,train_loss):
 if __name__ == "__main__":
     
     # Settings
-    epochs = 20
-    train_set_size = 48 
+    epochs = 50
+    train_set_size = 200
+    #test_set_size = 48
     lr = 0.01
+    batch_size = 1
 
     # Process args from command shell
     argv = os.sys.argv[1:]
@@ -102,9 +110,11 @@ if __name__ == "__main__":
 
     
     # Init training and test data
-    X = torch.zeros([train_set_size,HCS_dataset.num_features],dtype=torch.float32)
+    X = torch.zeros([train_set_size,HCS_dataset.num_features],
+                                         dtype=torch.float32)
+    X_test = torch.zeros([train_set_size,HCS_dataset.num_features],
+                                         dtype=torch.float32)
     Y = torch.zeros([train_set_size,1],dtype=torch.float32)
-    X_test = torch.zeros([train_set_size,HCS_dataset.num_features],dtype=torch.float32)
     Y_test = torch.zeros([train_set_size,1],dtype=torch.float32)
     for i in range(train_set_size):
         X[i],Y[i]  = HCS_dataset[i]
@@ -116,9 +126,16 @@ if __name__ == "__main__":
                         neuron_num=30,lr=lr)
     
     ## Train neural net with defined num of epochs, calc losses, plot data.
-    train_loss = np.zeros([epochs,train_set_size])
-    test_loss = np.zeros([epochs,train_set_size])
-    #plot = False
+    epoch_step_num = int(math.ceil(train_set_size/batch_size))
+    train_loss = np.zeros([epochs, epoch_step_num])
+    test_loss = np.zeros([epochs, epoch_step_num])
+    
+    logmsg("**")
+    logmsg("** Starting run for HCS {} with:".split('.')[0])
+    logmsg("** dataset samples num={}, trainset size={}, testset size={}," 
+               " batch size={}, epochs={}".format(
+               len(HCS_dataset), train_set_size, train_set_size, batch_size, epochs))
+    logmsg("**\n")
     for epoch in range(epochs):    
         
         '''plot = False
@@ -127,15 +144,17 @@ if __name__ == "__main__":
         permutations = torch.randperm(train_set_size)
         shuffled_X = X[permutations]
         shuffled_Y = Y[permutations]
-        train_loss[epoch] = nn.train_net(X=shuffled_X,Y=shuffled_Y,
-                                         plot=False,save_train_data=True)
-        epoch_total_loss,test_loss[epoch] = calc_mse_loss(X_test,Y_test,nn,pprint=False)
+        train_loss[epoch], test_loss[epoch] = \
+                     nn.train_net(X=shuffled_X, Y=shuffled_Y, plot=False, 
+                                  save_train_data=True, batch_size=batch_size)
+
+        epoch_total_loss, _ = calc_epoch_mse_loss(X_test,Y_test,nn, pprint=False)
         logmsg("epoch test loss: {}".format(epoch_total_loss))
 
 
-    final_test_mre_loss,_ = calc_mre_loss(X_test,Y_test,nn)
+    final_test_mre_loss,_ = calc_epoch_mre_loss(X_test,Y_test,nn)
     print("\n\n#####\n# Test MRE Loss:{}".format(final_test_mre_loss))
-    plot_loss(test_loss,train_loss)
+    #plot_mse_loss(test_loss, train_loss,ylabel="Root of MSE Loss",xlabel="Steps")
 
 
 
--- lib/nn_experimental.py
+++ lib/nn_experimental.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 from utils import logmsg
 
 ## Simply Deep NN design: 
@@ -12,9 +13,13 @@ class TrialNet(torch.nn.Module):
         super(TrialNet, self).__init__()
         self.hidden1 = torch.nn.Linear(obs_size, hidden_size)
         self.hidden2 = torch.nn.Linear(hidden_size, hidden2_size)
+        #self.hidden3 = torch.nn.Linear(hidden2_size, hidden2_size)
+        #self.hidden4 = torch.nn.Linear(hidden2_size, hidden2_size)
         self.output = torch.nn.Linear(hidden2_size, output_size)
         self.hidden1.weight.data.fill_(0.00)
         self.hidden2.weight.data.fill_(0.00)
+        #self.hidden3.weight.data.fill_(0.00)
+        #self.hidden4.weight.data.fill_(0.00)
         self.output.weight.data.fill_(0.00)
         #print(self.hidden1.weight)
         #print(self.hidden2.weight)
@@ -23,10 +28,11 @@ class TrialNet(torch.nn.Module):
     def forward(self, x):
         x = F.relu(self.hidden1(x))
         x = F.relu(self.hidden2(x))
+        #x = F.relu(self.hidden3(x))
+        #x = F.relu(self.hidden4(x))
         return self.output(x)
 
 class ExperimentalNN():
-
     def __init__(self, num_features, neuron_num, lr):
         
         self.input_dim = num_features
@@ -41,7 +47,7 @@ class ExperimentalNN():
     def predict(self, x):
         return self.net(x)
 
-
+    ## Train net for a single mini-batch step
     def train_step(self, x, y):
 
         # Make a prediction of the data using the network
@@ -49,7 +55,7 @@ class ExperimentalNN():
 
         # Calc loss
         loss = self.loss_fn(y_pred,y)
-        logmsg(" prediced Y:{}, Real Y:{} loss={}".format(y_pred,y,pow(loss,0.5)))
+        #logmsg(" predicted Y:{}, Real Y:{} loss={}".format(y_pred,y,pow(loss,0.5)))
 
         # Reset the Gradient Descent optimizer
         self.optimizer.zero_grad()
@@ -57,32 +63,52 @@ class ExperimentalNN():
         # Back progogate and let the GD optimizer update the parameters(weights)
         loss.backward()
         self.optimizer.step()
-        return loss,y_pred
+        return loss, y_pred.squeeze()
 
-
-    # Sample by sample training, TODO: add batch size
-    def train_net(self, X, Y, save_train_data=True, plot=False):
-        train_loss_for_step = torch.zeros(X.shape[0])
-        Y_pred = torch.zeros(X.shape[0])
-        for i,x in enumerate(X):
-            loss_at_step,Y_pred[i] = self.train_step(x,Y[i])
+    ## Train net for a single epoch
+    def train_net(self, X, Y, X_test=None, Y_test=None,save_train_data=True,
+                 calc_test_loss=False, plot=False, batch_size=8):
+        samples_num = X.shape[0]
+        steps_num = int(math.ceil(samples_num/batch_size))
+        Y_pred = torch.zeros(samples_num)
+        train_loss_for_step = torch.zeros(steps_num)
+        test_loss_for_step = torch.zeros(steps_num) if calc_test_loss else None
+        mse_loss_fn = torch.nn.MSELoss()
+        
+        batch_indexes  = range(batch_size, samples_num, batch_size)
+        batch_indexes  = list(batch_indexes) + [samples_num,]
+        prev_batch_end = 0
+        for i, batch_end in enumerate(batch_indexes):
+            batch_start = prev_batch_end 
+            loss_at_step,Y_pred[batch_start:batch_end] = \
+                                                    self.train_step(X[batch_start:batch_end,:],
+                                                        Y[batch_start:batch_end,:])
+            prev_batch_end = batch_end
             if save_train_data or plot:
                 train_loss_for_step[i] = loss_at_step
+                if calc_test_loss:
+                    Y_pred_test = self.net(X_test)
+                    test_loss_for_step[i] = mse_loss_fn(Y_pred_test, Y_test) 
+            if i+1 % 50:
+                logmsg("Done {} training steps".format(i+1))
+
+        
+        train_loss_for_step = train_loss_for_step.detach().numpy()
         if plot:
             fig, ax = plt.subplots(figsize=(12, 7))
             ax.set_title("Training loss progress")
             ax.set_xlabel('Loss')
             ax.set_ylabel('Steps')
-            ax.plot(np.arange(X.shape[0]),pow(train_loss_for_step.detach().numpy(),0.5),
+            ax.plot(np.arange(samples_num),pow(train_loss_for_step,0.5),
                     color="g",lw="2")
             plt.show()
      
         logmsg("------------------------------")
         logmsg("epoch train loss (root of MSE):{}"
-               .format(torch.norm((Y_pred-Y),2)/X.shape[0]))
+               .format(torch.norm((Y_pred-Y),2)/samples_num))
         
         if save_train_data:
-            return train_loss_for_step.detach().numpy()
+            return train_loss_for_step, test_loss_for_step
         else:
             return None
 
