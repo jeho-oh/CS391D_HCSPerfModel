49c5115d9c42d3032711963f155c751125fa5d4f
oleg zilman <zilmano@gmail.com>
2020-05-14 22:00:53 -0500

    .

--- bin/run_nn_cuda.py
+++ bin/run_nn_cuda.py
@@ -335,7 +335,7 @@ def configAxTLS():
     num_neurons = 30
     '''
     experiments=1
-    for train_set_size in [800,]:
+    for train_set_size in [7500,]:
         batch_size = int(math.ceil(train_set_size/4))
         results = []
         for i in range(experiments):
@@ -364,7 +364,7 @@ def configFiasco():
     num_neurons = 30
     '''
     experiments=1
-    for train_set_size in [7500,]:
+    for train_set_size in [1200,]:
         batch_size = int(math.ceil(train_set_size/4))
         results = []
         for i in range(experiments):
